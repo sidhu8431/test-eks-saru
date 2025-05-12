d07a079c154f0e43df4d3e4c62b7cc6d3d882ca0
sidhu8431 <siddharthadevops04@gmail.com>
2025-05-12 15:58:16 +0530

    Update rds.tf
     backup_retention_period = 7            # Keep backups for 7 days
      backup_window           = "00:00-01:00"  # Backup starts between midnight and 1 AM UTC

--- postgresql-db/rds.tf
+++ postgresql-db/rds.tf
@@ -15,6 +15,8 @@ resource "aws_db_instance" "logistics-postgres" {
   vpc_security_group_ids = var.db_security_group_ids
   skip_final_snapshot    = var.db_skip_final_snapshot
   deletion_protection     = var.db_deletion_protection
+  backup_retention_period = 7            # Keep backups for 7 days
+  backup_window           = "00:00-01:00"  # Backup starts between midnight and 1 AM UTC
   
  
   tags = {
