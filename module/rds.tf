8dc5892cfe21c84d8395cfae409e34144fd7f979
sidhu8431 <siddharthadevops04@gmail.com>
2025-05-12 16:09:02 +0530

    Update rds.tf

--- module/rds.tf
+++ module/rds.tf
@@ -1,32 +1,3 @@
-# module "rds" {
-#   source                  = "../postgresql-db"
-#   vpc_id                  = module.logstic_vpc.vpc_id
-#   db_identifier           = var.db_identifier
-#   db_engine               = var.db_engine
-#   db_engine_version       = var.db_engine_version
-#   db_instance_class       = var.db_instance_class
-#   db_allocated_storage    = var.db_allocated_storage
-#   db_storage_type         = var.db_storage_type
-#   db_name                 = var.db_name
-#   db_username             = var.db_username
-#   db_password             = var.db_password
-#   db_subnet_group_name    = var.db_subnet_group_name
-#   db_subnet_ids           = module.logstic_vpc.subnet_private_database[*].id
-#   db_parameter_group_name = var.db_parameter_group_name
-#   db_multi_az             = var.db_multi_az
-#   db_publicly_accessible  = var.db_publicly_accessible
-#   db_security_group_ids   = module.logistic.aws_security_group[*].id
-#   db_skip_final_snapshot  = var.db_skip_final_snapshot
-#   db_deletion_protection  = var.db_deletion_protection
-#   db_max_connections      = var.db_max_connections
-#   db_engine_family        = var.db_engine_family
-
-#    depends_on = [ module.logstic_vpc ]
-
-
-
-# }
-
 module "rds" {
   source                  = "../postgresql-db"
   vpc_id                  = module.logstic_vpc.vpc_id
@@ -51,4 +22,5 @@ module "rds" {
   db_deletion_protection = var.db_deletion_protection
   db_max_connections     = var.db_max_connections
   db_engine_family       = var.db_engine_family
+  multi_az = var.multi_az
 }
