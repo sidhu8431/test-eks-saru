363d0d2e59a50f3fda1291ed3872336c1b87f05f
sidhu8431 <siddharthadevops04@gmail.com>
2025-05-22 14:53:45 +0530

    Update eks.tf

--- module/eks.tf
+++ module/eks.tf
@@ -56,10 +56,12 @@
 # }
 module "eks" {
   source = "../aws-eks-terraform"
- 
+
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
   vpc_id          = module.logstic_vpc.vpc_id
   subnet_ids      = module.logstic_vpc.subnet_private
   node_groups     = var.node_groups
+
+  environment = var.environment   # <-- Add this line
 }
