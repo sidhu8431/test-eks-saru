91cef3a25f4eb85fbbe50fa72e9f10b6b47b8d70
sidhu8431 <siddharthadevops04@gmail.com>
2025-05-22 14:42:41 +0530

    Update sg.tf

--- terraform-sg/sg.tf
+++ terraform-sg/sg.tf
@@ -1,6 +1,7 @@
 resource "aws_security_group" "allow_all_sg" {
   count  = length(var.sg_name)
   vpc_id = var.vpc_id
+  environment       = var.environment
 
   egress {
     from_port   = var.from_port
