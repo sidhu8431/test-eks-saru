22b618d684d0b26335cd23986d1369a83158fc79
sidhu8431 <siddharthadevops04@gmail.com>
2025-05-22 14:34:01 +0530

    Update subnet.tf

--- terraform-vpc/subnet.tf
+++ terraform-vpc/subnet.tf
@@ -1,43 +1,48 @@
 resource "aws_subnet" "logistics_public_subnet" {
-  count      = length(var.subnet_public)
-  vpc_id     = aws_vpc.logstic_vpc.id
-  cidr_block = var.subnet_public[count.index]
-  availability_zone = local.az_avail_zone[count.index]
- # map_customer_owned_ip_on_launch = true
- map_public_ip_on_launch = true
+  count                   = length(var.subnet_public)
+  vpc_id                  = aws_vpc.logstic_vpc.id
+  cidr_block              = var.subnet_public[count.index]
+  availability_zone       = local.az_avail_zone[count.index]
+  map_public_ip_on_launch = true
+
   tags = merge(
     var.tags_common,
     var.subnet_public_tag,
     {
-      Name = "${var.project}-public-subnet-${local.az_avail_zone[count.index]}-${count.index + 1}"
+      Name        = "${var.project}-${var.environment}-public-subnet-${local.az_avail_zone[count.index]}-${count.index + 1}"
+      Environment = var.environment
     }
   )
 }
 
 resource "aws_subnet" "logistics_private_subnet" {
-  count      = length(var.subnet_private)
-  vpc_id     = aws_vpc.logstic_vpc.id
-  cidr_block = var.subnet_private[count.index]
-    availability_zone = local.az_avail_zone[count.index]
+  count             = length(var.subnet_private)
+  vpc_id            = aws_vpc.logstic_vpc.id
+  cidr_block        = var.subnet_private[count.index]
+  availability_zone = local.az_avail_zone[count.index]
+
   tags = merge(
     var.tags_common,
     var.subnet_private_tag,
     {
-      Name = "${var.project}-private-subnet-${local.az_avail_zone[count.index]}-${count.index + 1}"
+      Name        = "${var.project}-${var.environment}-private-subnet-${local.az_avail_zone[count.index]}-${count.index + 1}"
+      Environment = var.environment
     }
   )
 }
 
 resource "aws_subnet" "logistics_database_subnet" {
-  count      = length(var.subnet_private_database)
-  vpc_id     = aws_vpc.logstic_vpc.id
-  cidr_block = var.subnet_private_database[count.index]
-    availability_zone = local.az_avail_zone[count.index]
+  count             = length(var.subnet_private_database)
+  vpc_id            = aws_vpc.logstic_vpc.id
+  cidr_block        = var.subnet_private_database[count.index]
+  availability_zone = local.az_avail_zone[count.index]
+
   tags = merge(
     var.tags_common,
     var.subnet_private_database_tag,
     {
-      Name = "${var.project}-priavte_database-subnet-${local.az_avail_zone[count.index]}-${count.index + 1}"
+      Name        = "${var.project}-${var.environment}-private-database-subnet-${local.az_avail_zone[count.index]}-${count.index + 1}"
+      Environment = var.environment
     }
   )
 }
