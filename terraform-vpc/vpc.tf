d722086639cb1c0db9ed391bb1481baf1d6b26da
sidhu8431 <siddharthadevops04@gmail.com>
2025-05-22 14:35:03 +0530

    Update vpc.tf

--- terraform-vpc/vpc.tf
+++ terraform-vpc/vpc.tf
@@ -1,11 +1,12 @@
 resource "aws_vpc" "logstic_vpc" {
   cidr_block           = var.cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
-  
+
   tags = merge(
     var.tags_common,
     {
-      Name = local.resource_name
+      Name        = "${var.project}-${var.environment}-vpc"
+      Environment = var.environment
     }
   )
 }
