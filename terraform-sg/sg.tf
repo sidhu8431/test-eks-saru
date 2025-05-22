a0f31da613d473b24f2c790c7a376c23ed04ec18
sidhu8431 <siddharthadevops04@gmail.com>
2025-05-22 14:28:36 +0530

    Update sg.tf

--- terraform-sg/sg.tf
+++ terraform-sg/sg.tf
@@ -1,26 +1,23 @@
 resource "aws_security_group" "allow_all_sg" {
-  # ... other configuration ...
-  count = length(var.sg_name)
+  count  = length(var.sg_name)
   vpc_id = var.vpc_id
+
   egress {
     from_port   = var.from_port
     to_port     = var.to_port
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
+
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
-
 
   tags = {
-    Name = "${var.sg_name[count.index]}"
+    Name        = "${var.sg_name[count.index]}-${var.environment}"
+    Environment = var.environment
   }
-
-
 }
