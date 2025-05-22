13a9c194c05d644d98ee3f33b7648e7859cac554
sidhu8431 <siddharthadevops04@gmail.com>
2025-05-22 14:31:40 +0530

    Update igw.tf

--- terraform-vpc/igw.tf
+++ terraform-vpc/igw.tf
@@ -2,37 +2,28 @@ resource "aws_internet_gateway" "logistics-igw" {
   vpc_id = aws_vpc.logstic_vpc.id
 
   tags = {
-    Name = local.igw_name
+    Name        = "${var.project}-${var.environment}-igw"
+    Environment = var.environment
   }
 }
 
 resource "aws_eip" "nat_eip" {
-#   vpc = true
-# count = length(var.subnet_public)
-   domain = "vpc"
+  domain = "vpc"
 
   tags = {
-    Name = "NAT-Gateway-EIP"
+    Name        = "${var.project}-${var.environment}-nat-eip"
+    Environment = var.environment
   }
 }
 
-
 resource "aws_nat_gateway" "nat_gw" {
-  # count = length(var.subnet_public)
- # allocation_id = aws_eip.nat_eip[count.index].id
   allocation_id = aws_eip.nat_eip.id
-  subnet_id      = aws_subnet.logistics_public_subnet[0].id
-#  subnet_id      = aws_subnet.logistics_public_subnet[count.index].id
-#  count         = length(var.subnet_public)
-# allocation_id = aws_eip.nat_eip[count.index].id
-#   subnet_id     = var.subnet_public[count.index] # Use the subnet ID directly
-   
+  subnet_id     = aws_subnet.logistics_public_subnet[0].id
 
   tags = {
-    Name = "${var.project}-${var.environment}-NAT_GW"
+    Name        = "${var.project}-${var.environment}-nat-gw"
+    Environment = var.environment
   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.logistics-igw]
 }
